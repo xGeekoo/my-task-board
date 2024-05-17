da7775f2a7d75b0c0e433bead2bae5d2255cefc4
xGeekoo <64699089+xGeekoo@users.noreply.github.com>
2024-05-17 15:42:30 -0400

    create add new task button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
+import TaskForm from './components/TaskForm';
 import logoSVG from './assets/Logo.svg';
 import crossSVG from './assets/close_ring_duotone.svg';
 import pencilSVG from './assets/Edit_duotone.svg';
-import TaskForm from './components/TaskForm';
+import plusSVG from './assets/Add_round_duotone.svg';
 
 function App() {
   const [showTaskForm, setShowTaskForm] = useState(false);
@@ -25,27 +26,38 @@ function App() {
       </header>
 
       <main>
-        <article
+        <div className="mb-4">
+          <article
+            onClick={() => setShowTaskForm(true)}
+            className="relative flex cursor-pointer items-start gap-x-6 rounded-xl bg-clr-gray-light p-4"
+          >
+            <div>
+              <span className="mb-2 flex aspect-square w-11 shrink-0 items-center justify-center rounded-xl bg-clr-white">
+                📚
+              </span>
+              <span className="right-4 top-4 flex aspect-square w-11 shrink-0 items-center justify-center rounded-xl bg-clr-red-dark sm:absolute">
+                <img src={crossSVG} alt="Red X" />
+              </span>
+            </div>
+            <div className="self-center">
+              <h3 className="text-xl font-semibold">Task To Do</h3>
+              <p className="mt-1 font-light leading-5">
+                Work on a Challenge on devChallenges.io,
+                <br />
+                learn TypeScript.
+              </p>
+            </div>
+          </article>
+        </div>
+        <div
           onClick={() => setShowTaskForm(true)}
-          className="relative flex cursor-pointer items-start gap-x-6 rounded-xl bg-clr-gray-light p-4"
+          className="flex cursor-pointer items-center gap-x-6 rounded-xl bg-clr-orange-light p-4"
         >
-          <div>
-            <span className="mb-2 flex aspect-square w-11 shrink-0 items-center justify-center rounded-xl bg-clr-white">
-              📚
-            </span>
-            <span className="right-4 top-4 flex aspect-square w-11 shrink-0 items-center justify-center rounded-xl bg-clr-red-dark sm:absolute">
-              <img src={crossSVG} alt="Red X" />
-            </span>
-          </div>
-          <div className="self-center">
-            <h3 className="text-xl font-semibold">Task To Do</h3>
-            <p className="mt-1 font-light leading-5">
-              Work on a Challenge on devChallenges.io,
-              <br />
-              learn TypeScript.
-            </p>
-          </div>
-        </article>
+          <span className="flex aspect-square w-11 shrink-0 items-center justify-center rounded-xl bg-clr-orange-dark">
+            <img src={plusSVG} alt="" />
+          </span>
+          <h3 className="font-semibold">Add new task</h3>
+        </div>
       </main>
       {showTaskForm && <TaskForm setShowTaskForm={setShowTaskForm} />}
     </div>
